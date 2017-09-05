3e1dd48d8e4154e454b4e8e9e78f79ef41dad2ef
wuwenxue <yxiaopangy@163.com>
2017-09-05 15:00:23 +0800

    必须的

---
+++ sourceTreeUsage/src/main/java/com/wwx/App.java
@@ -0,0 +1,13 @@
+package com.wwx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
